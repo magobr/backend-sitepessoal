fc7dd1fb8f1ff59daa8a075397b3d4291fe6d43f
Thiago <novaesthago@gmail.com>
2020-04-23 13:25:57 -0300

    Commit para BKP

--- src/routes.js
+++ src/routes.js
@@ -8,20 +8,26 @@ const dataUpdate = require('./models/update');
 const routes = express.Router();
 
 routes.get('/users', (req, res) => {
-    select.getDados((err, response) =>{
-        res.json(response)
-    });
+        select.getDados((err, response) =>{
+            if(err)throw err;
+            res.json(response)
+        });
 });
 
 routes.get('/users/:id?', (req, res) =>{
+
     console.log(req.params.id);
     select.getDadosById(req.params.id, (err, response) =>{
+        if(err) throw err
         res.json(response)
     });
+
 });
 
 routes.post('/login', (req, res) =>{
+
     var dados = req.body;
+
     select.getLogin(dados, (err, response) =>{
         if (response.length > 0){
             req.session.loggedin = true;
@@ -32,28 +38,35 @@ routes.post('/login', (req, res) =>{
         } else {
             console.log('login incorreto');
             res.end();
+        } if (err){
+            throw err;
         }
-
-        
     })
 })
 
 routes.post('/saveuser', (req, res) => {
+
     var dados = req.body;
+
     console.log(req.session.loggedin);
+
     if (req.session.loggedin){
         create.userCreate(dados, (err, response) => {
-            res.json(req.body);
+            if (err) throw err;
+            res.json(response);
             console.log(dados);
         });
     } else{
         console.log('Faça o login');
         res.end()
     }
+
 });
 
 routes.delete('/usersDelete/:id', (req, res) =>{
+
     var id = req.params.id; 
+
     if(req.session.loggedin){
         dataDelete.userDelete(id, (err, count) =>{
             console.log(id);
@@ -63,14 +76,24 @@ routes.delete('/usersDelete/:id', (req, res) =>{
         console.log('Faça o login');
         res.end()
     }
+
 })
 
 routes.put('/usersUpdate/:id', (req, res) => {
+    
+
     var id = req.params.id;
     var data = req.body
-    dataUpdate.userUpdate(data, id, (err, response) =>{
-        res.json(response);
-    });
+
+    if(req.session.loggedin){
+        dataUpdate.userUpdate(data, id, (err, response) =>{
+            res.json(response);
+        });
+    } else{
+        console.log('Faça o login');
+        res.end()
+    }
+
 });
 
 module.exports = routes;
